2c7fa273f208c95ef6cabeab331c91a6a2a674b6
Isaac Maya <isaacmaya@MacBook-Air-de-Isaac.local>
2024-08-16 17:31:12 -0600

    Premiere commit

---
+++ setup_api_agent.sh
@@ -0,0 +1,1 @@
+ansible-playbook -vv -i hosts.ini ansible_playbooks/build_jenkins_api_agent.yml --ask-become-pass
---
+++ setup_ui_agent.sh
@@ -0,0 +1,1 @@
+ansible-playbook -vvv -i hosts.ini ansible_playbooks/build_jenkins_ui_agent.yml --ask-become-pass
