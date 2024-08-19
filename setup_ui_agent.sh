48169a0a9281466b7dbd430f79807eb81ac4f17e
Isaac Maya <theisaacmaya@icloud.com>
2024-08-19 15:37:37 -0600

    UI pipeline now working

--- setup_ui_agent.sh
+++ setup_ui_agent.sh
@@ -1,1 +1,1 @@
-ansible-playbook -vvv -i hosts.ini ansible_playbooks/build_jenkins_ui_agent.yml --ask-become-pass
+ansible-playbook -vv -i hosts.ini ansible_playbooks/build_jenkins_ui_agent.yml --ask-become-pass
